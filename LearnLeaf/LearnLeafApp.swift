c4481edadf21ab02222045ebd88b56cbe820006d
Benjamin Niccum <btn1609@mavs.uta.edu>
2024-11-30 21:38:32 -0600

    Initial Commit

---
+++ LearnLeaf/LearnLeafApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnLeafApp.swift
+//  LearnLeaf
+//
+//  Created by Ben Niccum on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnLeafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearnLeafTests/LearnLeafTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnLeafTests.swift
+//  LearnLeafTests
+//
+//  Created by Ben Niccum on 11/30/24.
+//
+
+import Testing
+@testable import LearnLeaf
+
+struct LearnLeafTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
